ef315c861f82cbd975c9e38852e3c7a8b75c9bbb
Prashant Bhawsar <prashant.bhawsar@nagarro.com>
2024-03-22 07:06:12 +0530

    mern app

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-auth-1c4ae.firebaseapp.com',
-  projectId: 'mern-auth-1c4ae',
-  storageBucket: 'mern-auth-1c4ae.appspot.com',
-  messagingSenderId: '277641423672',
-  appId: '1:277641423672:web:2de25252aae022d51aafcd',
+  authDomain: "mern-auth-ddd00.firebaseapp.com",
+  projectId: "mern-auth-ddd00",
+  storageBucket: "mern-auth-ddd00.appspot.com",
+  messagingSenderId: "1026767443350",
+  appId: "1:1026767443350:web:fb6d041d66f9b6c9cfb9b7",
 };
 
 // Initialize Firebase
